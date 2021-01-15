d0f7fe2bd300ed79b263272f2fb16118b22e6e49
Bryden Uyehara <brydenuyehara@gmail.com>
2021-01-14 17:29:16 -0800

    created register and login user forms

--- forms.py
+++ forms.py
@@ -1,3 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequred, Length
+
+class CreateUserForm(FlaskForm):
+    """Form for registering a new user"""
+
+    username = StringField('Username', validators=[DataRequred()])
+    password = PasswordField('Password', validators=[DataRequred()])
+    image_url = StringField('(Optional) Image URL')
+
+class LoginForm(FlaskForm):
+    """Login user form"""
+
+    username = StringField('Username', validators=[DataRequred()])
+    password = PasswordField('Password', validators=[DataRequred()])
