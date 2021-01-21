06439070f066b362115e254f9a46a55c4335774b
Bryden Uyehara <brydenuyehara@gmail.com>
2021-01-20 16:31:13 -0800

    updated forms

--- forms.py
+++ forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequred, Length
+from wtforms.validators import DataRequired, Length
 
 class CreateUserForm(FlaskForm):
     """Form for registering a new user"""
 
-    username = StringField('Username', validators=[DataRequred()])
-    password = PasswordField('Password', validators=[DataRequred()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     image_url = StringField('(Optional) Image URL')
 
 class LoginForm(FlaskForm):
     """Login user form"""
 
-    username = StringField('Username', validators=[DataRequred()])
-    password = PasswordField('Password', validators=[DataRequred()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
