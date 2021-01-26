c559f4010acdc2de4c388018372acca7b44e0af5
Bryden Uyehara <brydenuyehara@gmail.com>
2021-01-25 16:58:05 -0800

    added new form for lists

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, Length
+
 
 class CreateUserForm(FlaskForm):
     """Form for registering a new user"""
@@ -9,8 +10,17 @@ class CreateUserForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     image_url = StringField('(Optional) Image URL')
 
+
 class LoginForm(FlaskForm):
     """Login user form"""
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+
+class ListForm(FlaskForm):
+    """Create/modify list form"""
+
+    title = StringField('Title', validators=[DataRequired()])
+    ranked = BooleanField('Ranked ')
+    private = BooleanField('Private')
