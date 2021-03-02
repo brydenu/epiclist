ed64641eca3e3f82c8d0574a967a94e7c5eb720e
Bryden Uyehara <brydenuyehara@gmail.com>
2021-03-02 14:12:23 -0800

    fixed error

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from models import db, connect_db, User, Follows, Character, List, ListCharacter
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] == os.environ.get("DATABASE_URL", "postgres:///epiclist")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgres:///epiclist")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
